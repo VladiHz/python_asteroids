a164679bf0d08e1e5bd827934a03cf67d875f7ce
VladiHz <vladimir.hernandez@gmail.com>
2025-11-03 17:22:58 -0400

    Shots kill asteroids

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ def main():
             if asteroid.collision(player):
                 print("Game Over!")
                 sys.exit(0)
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black ")
 
